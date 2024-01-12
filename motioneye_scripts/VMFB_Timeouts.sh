825c881c1613b909ee6baf3cfc92d4b834e4e4ad
src1138 <src1138@gmail.com>
2024-01-12 23:54:41 +0100

    a little more progress on motioneye scripts - still not ready

--- motioneye_scripts/VMFB_Timeouts.sh
+++ motioneye_scripts/VMFB_Timeouts.sh
@@ -12,58 +12,89 @@ pbka_interval=10
 pbka_pulse_length=1
 
 # Set up pins for Sensor IR and Motor
+# Assign pins
+PIR=27 
+SIRC=25
+MTR=11
 
-#THIS SCRIPT NEEDS TO SHUT OFF THE SENSORS 30 SEC AFTER LAST PIR TRIGGER AND THE MOTOR 10 SEC AFTER LAST DEPOSIT TRIGGER
+# Verify they are set up, else initialize them
+test -e /sys/class/gpio/gpio$PIR ||
+  (echo $PIR > /sys/class/gpio/export \
+   && echo in > /sys/class/gpio/gpio$PIR/direction)
+test -e /sys/class/gpio/gpio$SIR ||
+  (echo $SIR > /sys/class/gpio/export \
+   && echo out > /sys/class/gpio/gpio$SIR/direction)
+test -e /sys/class/gpio/gpio$MTR ||
+  (echo $MTR > /sys/class/gpio/export \
+   && echo out > /sys/class/gpio/gpio$MTR/direction)
 
 # Sensors
-# check if sensors are on, if not, exit
-# check if PIR is triggering - if so, exit
-# parse log for last PIR trigger and get datetime
-# subtract last PIR trigger datetime from now
-# if result is >= sensor_timeout, turn sensors off
-#	update previous value file
-# 	log sensor timeout event
+valPIR=$(cat /sys/class/gpio/gpio$PIR/value)
+valSIR=$(cat /sys/class/gpio/gpio$SIR/value)
+# check that sensors are on
+if [ "$valSIR" == "1" ]; then
+	# check that PIR is not triggering 
+	if [ "$valPIR" == "0" ]; then
+		# parse log for last PIR trigger and get datetime
+		# subtract last PIR trigger datetime from now
+		# if result is >= sensor_timeout, 
+			# turn sensors off
+			echo "0" >| /sys/class/gpio/gpio$SIR/value
+			# update previous value file
+			echo "0" >| /data/log/prev_valSIR
+			# log sensor timeout event
+	fi
+fi
 
 # Motor
-# check if motor is on, if not, exit
-# parse log for last Deposit trigger and get datetime
-# subtract last Deposit trigger datetime from now
-# if result is >= motor_timeout, turn motor off
-#	update previous value file
-# 	log motor timeout event
+valMTR=$(cat /sys/class/gpio/gpio$MTR/value)
+# check that motor is on
+if [ "$valMTR" == "1" ]; then
+	# parse log for last Deposit trigger and get datetime
+	# subtract last Deposit trigger datetime from now
+	# if result is >= motor_timeout, 
+		# turn motor off
+		echo "0" >| /sys/class/gpio/gpio$MTR/value
+		# update previous value file
+		echo "0" >| /data/log/prev_valMTR
+		# log motor timeout event
+fi
 
 #PBKA
 
-# check if PBKA is enabled (PBKA_enable=$(cat /data/log/PBKA_enable == 1) else exit
-# check if PIR is triggering - if so, exit
+# check that PBKA is enabled
+if [ $(cat /data/log/PBKA_enable == 1) ] then
 
-# PBKA Interval
+	# PBKA Interval
 
-# parse log for last PIR trigger event and get datetime
-# subtract last PIR trigger datetime from now+pbka_pulse_length
-# if result is >= sensor_timeout+pbka_interval, continue, else exit
+	# parse log for last PIR trigger event and get datetime
+	# subtract last PIR trigger datetime from now+pbka_pulse_length
+	# if result is >= sensor_timeout+pbka_interval, continue, else exit
 
-# PBKA Pulse
+		# PBKA Pulse
 
-# if sensors are off
-# parse log for last PBKA pulse off event and get datetime - 
-# 	if no PBKA pulse event log for today, turn on sensors
-#		update previous value file
-# 		log PBKA on event
-# 	else subtract pbka_pulse_length from now
-# 		if result is >= pbka_interval, turn on sensors
-#			update previous value file
-# 			log PBKA on event
-
-# if sensors are on
-# parse log for last PBKA pulse on event and get datetime
-# 	if no PBKA pulse event log for today, turn off sensors
-#		update previous value file
-# 		log PBKA off event
-# 	else subtract last PBKA pulse event datetime from now
-# 		if result is >= pbka_pulse_length, turn off sensors
-#			update previous value file
-# 			log PBKA off event
-
+		# check that Sensors are off 
+		if [ "$valSIR" == "0" ]; then
+			# parse log for last PBKA pulse off event and get datetime - 
+			# if no PBKA pulse event log for today
+				#turn on sensors
+				# update previous value file
+				# log PBKA on event
+			# else subtract pbka_pulse_length from now
+				# if result is >= pbka_interval, turn on sensors
+				# update previous value file
+				# log PBKA on event
+		else # sensors are on
+			# parse log for last PBKA pulse on event and get datetime
+			# if no PBKA pulse event log for today, turn off sensors
+			# update previous value file
+			# log PBKA off event
+			# else subtract last PBKA pulse event datetime from now
+			# if result is >= pbka_pulse_length, turn off sensors
+			# update previous value file
+			# log PBKA off event
+		fi
+	fi
+fi
 
 # sleep 1 sec
--- motioneye_scripts/VMFB_Timer.sh
+++ motioneye_scripts/VMFB_Timer.sh
@@ -27,25 +27,28 @@ test -e /sys/class/gpio/gpio$MTRCON ||
 
 while true
 do
-# check if Timer is enabled (Timer_enable=$(cat /data/log/PBKA_enable == 1) else exit
+# check if Timer is enabled 
+if [ $(cat /data/log/Timemr_enable) == "1" ] then
 
-# Get the current time's hours and minutes (ex. 1430 for 2:30pm)
-nowtime=$(date +%H%M)
+	# Get the current time's hours and minutes (ex. 1430 for 2:30pm)
+	nowtime=$(date +%H%M)
 
-# Swap $ontime and $offtime to disable early and enable late
-if [[ "$nowtime" > "$timer_ontime" ]]; then
-	if [[ "$nowtime" < "$timer_offtime" ]]; then
-		VMFB_logfile="/data/log/VMFB_$(date +%F).log"
-		touch "$VMFB_logfile"
-		echo "$(date +%F_%X)	TMR	START">> "$VMFB_logfile"
-		# Set the sensor IR and motor pins to high
-		echo "1">/sys/class/gpio/gpio$SIRCON/value
-		echo "1">/sys/class/gpio/gpio$MTRCON/value
-		# Update previous value files
-		echo $valSIR >| /data/log/prev_valSIR
-		echo $valMTR >| /data/log/prev_valMTR
+	# Swap $ontime and $offtime to disable early and enable late
+	if [[ "$nowtime" > "$timer_ontime" ]]; then
+		if [[ "$nowtime" < "$timer_offtime" ]]; then
+			VMFB_logfile="/data/log/VMFB_$(date +%F).log"
+			touch "$VMFB_logfile"
+			echo "$(date +%F_%X)	TMR	START">> "$VMFB_logfile"
+			# Set the sensor IR and motor pins to high
+			echo "1">/sys/class/gpio/gpio$SIRCON/value
+			echo "1">/sys/class/gpio/gpio$MTRCON/value
+			# Update previous value files
+			echo $valSIR >| /data/log/prev_valSIR
+			echo $valMTR >| /data/log/prev_valMTR
+		fi
 	fi
 fi
+
 # Trigger timer every timer_interval
 sleep timer_interval
 
