bb9bbec7ecdac9521159c30467e8d1408c66e5cd
src1138 <src1138@gmail.com>
2024-01-12 16:45:44 +0100

    a little more progress on motioneye scripts - still not ready

--- motioneye_scripts/VMFB_Timer.sh
+++ motioneye_scripts/VMFB_Timer.sh
@@ -13,14 +13,6 @@ timer_ontime="0600" #6:00am
 # time of day for timer to stop
 timer_offtime="1800" #6:00pm
 
-
-# Assumes you want to enable the camera early and disable it late
-# To do the opposite, you can swap the $ontime and $offtime variables below
-
-# Set the time you want to enable and disable the camera
-ontime="0600" #6:00am
-offtime="1800" #6:00pm
-
 # Set pins 11 and 25 to be outputs driving low to avoid them going high on start up.
 # Add the following line to your /boot/config.txt
 # gpio=11,25=op,dl
@@ -40,24 +32,17 @@ do
 # Get the current time's hours and minutes (ex. 1430 for 2:30pm)
 nowtime=$(date +%H%M)
 
-# Get the target on/off state
-targetState="0"
 # Swap $ontime and $offtime to disable early and enable late
-if [[ "$nowtime" > "$ontime" ]]; then
-    if [[ "$nowtime" < "$offtime" ]]; then
-        targetState="1"
-    fi
+if [[ "$nowtime" > "$timer_ontime" ]]; then
+	if [[ "$nowtime" < "$timer_offtime" ]]; then
+		VMFB_logfile="/data/log/VMFB_$(date +%F).log"
+		touch "$VMFB_logfile"
+		echo "$(date +%F_%X)	TMR	START">> "$VMFB_logfile"
+		# Set the sensor IR and motor pins to high
+		echo "1">/sys/class/gpio/gpio$SIRCON/value
+		echo "1">/sys/class/gpio/gpio$MTRCON/value
+	fi
 fi
-
-if [ "$targetState"=="1" ]; then
-	VMFB_logfile="/data/log/VMFB_$(date +%F).log"
-	touch "$VMFB_logfile"
-	echo "$(date +%F_%X)	TMR	START">> "$VMFB_logfile"
-	# Set the sensor IR and motor pins to high
-	echo "1">/sys/class/gpio/gpio$SIRCON/value
-	echo "1">/sys/class/gpio/gpio$MTRCON/value
-fi
-
 # Trigger timer every timer_interval
 sleep timer_interval
 
