d9b7ef48a8ef518f525b0bcf6084077ed7046617
src1138 <src1138@gmail.com>
2024-01-12 19:14:33 +0100

    a little more progress on motioneye scripts - still not ready

--- motioneye_scripts/VMFB_Timeouts.sh
+++ motioneye_scripts/VMFB_Timeouts.sh
@@ -11,24 +11,24 @@ pbka_interval=10
 # powerbank keep-alive pulse length in seconds
 pbka_pulse_length=1
 
+# Set up pins for Sensor IR and Motor
+
 #THIS SCRIPT NEEDS TO SHUT OFF THE SENSORS 30 SEC AFTER LAST PIR TRIGGER AND THE MOTOR 10 SEC AFTER LAST DEPOSIT TRIGGER
 
 # Sensors
 # check if sensors are on, if not, exit
 # check if PIR is triggering - if so, exit
-# sensors are on and PIR is not triggering
 # parse log for last PIR trigger and get datetime
 # subtract last PIR trigger datetime from now
 # if result is >= sensor_timeout, turn sensors off
-# log sensor timeout event
+# 	log sensor timeout event
 
 # Motor
 # check if motor is on, if not, exit
-# motor is on
 # parse log for last Deposit trigger and get datetime
 # subtract last Deposit trigger datetime from now
 # if result is >= motor_timeout, turn motor off
-# log motor timeout event
+# 	log motor timeout event
 
 #PBKA
 
@@ -38,20 +38,26 @@ pbka_pulse_length=1
 # PBKA Interval
 
 # parse log for last PIR trigger event and get datetime
-# need to get sensor_timeout value - maybe add this to the timeouts script, but for now we assume we can get it
 # subtract last PIR trigger datetime from now+pbka_pulse_length
 # if result is >= sensor_timeout+pbka_interval, continue, else exit
 
 # PBKA Pulse
 
 # if sensors are off
-# parse log for last PBKA pulse event and get datetime
-# subtract last PBKA pulse event datetime+pbka_pulse_length from now
-# if result is >= pbka_interval, turn on sensors
-# log PBKA on event
+# parse log for last PBKA pulse off event and get datetime - 
+# 	if no PBKA pulse event log for today, turn on sensors
+# 		log PBKA on event
+# 	else subtract pbka_pulse_length from now
+# 		if result is >= pbka_interval, turn on sensors
+# 			log PBKA on event
 
 # if sensors are on
-# parse log for last PBKA pulse event and get datetime
-# subtract last PBKA pulse event datetime from now
-# if result is >= pbka_pulse_length, turn off sensors
-# log PBKA off event
+# parse log for last PBKA pulse on event and get datetime
+# 	if no PBKA pulse event log for today, turn off sensors
+# 		log PBKA off event
+# 	else subtract last PBKA pulse event datetime from now
+# 		if result is >= pbka_pulse_length, turn off sensors
+# 			log PBKA off event
+
+
+# sleep 1 sec
