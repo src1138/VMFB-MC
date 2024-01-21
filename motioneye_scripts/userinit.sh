105b83aab41a31b57fc4a406a3c582ef9f56e711
src1138 <src1138@gmail.com>
2024-01-21 13:19:37 +0100

    added PBKA script

--- motioneye_scripts/userinit.sh
+++ motioneye_scripts/userinit.sh
@@ -1,12 +1,14 @@
 # Turn off HDMI to save ~24mA
 /usr/bin/tvservice -o >> /data/log/userinit.log
-# Start deposit/dispense monitor/logger
+# Start deposit/dispense 
 nohup /data/etc/VMFB_Deposit-Dispense.py >> /data/log/userinit.log &
-# Start PIR, Sensor IR and Empty sensors monitor/logger
+# Start PIR, Sensor IR and Empty sensors
 nohup /data/etc/VMFB_Sensors.sh >> /data/log/userinit.log &
-# Start Sensor IR, Motor and PBKA timeout monitor/logger
+# Start Sensor IR and Motor timeout
 nohup /data/etc/VMFB_Timeouts.sh >> /data/log/userinit.log &
-# Start dispense timer monitor/logger
+# Start dispense timer
 nohup /data/etc/VMFB_Timer.sh >> /data/log/userinit.log &
+# Start PBKA 
+nohup /data/etc/VMFB_PBKA.sh >> /data/log/userinit.log &
 # Pause motion detection on startup
 curl http://localhost:7999/1/detection/pause >> /data/log/userinit.log
