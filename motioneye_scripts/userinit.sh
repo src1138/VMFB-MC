09c69443d1deb6d584fb6fdad9ffb72a69016de2
src1138 <src1138@gmail.com>
2024-03-27 22:49:46 +0100

    Adding configurable startup state for PBKA and timed dispense.

--- motioneye_scripts/userinit.sh
+++ motioneye_scripts/userinit.sh
@@ -4,6 +4,8 @@
 nohup /data/etc/VMFB_GPIO_Init.sh >> /data/log/userinit.log &
 # Start deposit/dispense 
 nohup /data/etc/VMFB-MC.py >> /data/log/userinit.log &
+# Initialize PBKA and timed dispense state
+nohup /data/etc/VMFB_PBKA-Timer_Init.sh >> /data/log/userinit.log &
 # Pause motion detection on startup and stop any recording that may have started
 curl http://localhost:7999/1/detection/pause >> /data/log/userinit.log
 curl http://localhost:7999/1/action/eventend >> /data/log/userinit.log
