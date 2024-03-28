6bb6380b9264b2550628926f85dbee3596af8999
src1138 <src1138@gmail.com>
2024-03-28 23:44:39 +0100

    Fixed windows newline, updated comment

--- motioneye_scripts/VMFB_PBKA-Timer_Init.sh
+++ motioneye_scripts/VMFB_PBKA-Timer_Init.sh
@@ -10,8 +10,8 @@
 
 # If you want timed dispense enabled on startup 
 # (in case of reboot, etc.), uncomment the following five lines
-# TMR_E=16 #RPi ZeroW Pin #36
-# test -e /sys/class/gpio/gpio$TMR_E ||
-#  (echo $TMR_E > /sys/class/gpio/export \
-#   && echo out > /sys/class/gpio/gpio$TMR_E/direction)
-# echo "1" >| /sys/class/gpio/gpio$TMR_E/value
+TMR_E=16 #RPi ZeroW Pin #36
+test -e /sys/class/gpio/gpio$TMR_E ||
+ (echo $TMR_E > /sys/class/gpio/export \
+ && echo out > /sys/class/gpio/gpio$TMR_E/direction)
+echo "1" >| /sys/class/gpio/gpio$TMR_E/value
