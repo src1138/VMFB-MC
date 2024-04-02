747f1b646f91bbd62ee8cb5ce86b28c6d7982521
src1138 <src1138@gmail.com>
2024-04-02 13:24:13 +0200

    Moved PBKA and timed dispense init to VMFB-MC.py

--- motioneye_scripts/userinit.sh
+++ motioneye_scripts/userinit.sh
@@ -4,8 +4,6 @@
 nohup /data/etc/VMFB_GPIO_Init.sh >> /data/log/userinit.log &
 # Start the main program to read sensors and control motor and IR LEDs 
 nohup /data/etc/VMFB-MC.py >> /data/log/userinit.log &
-# Initialize PBKA and timed dispense state
-nohup /data/etc/VMFB_PBKA-Timer_Init.sh >> /data/log/userinit.log &
 # Pause motion detection on startup and stop any recording that may have started
 curl http://localhost:7999/1/detection/pause >> /data/log/userinit.log
 curl http://localhost:7999/1/action/eventend >> /data/log/userinit.log
