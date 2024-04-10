2fe22537384122aa84753a9e538a2b1f39a5f91e
src1138 <src1138@gmail.com>
2024-04-10 23:35:48 +0200

    logging cleanup

--- motioneye_scripts/logMotionEventEnd.sh
+++ motioneye_scripts/logMotionEventEnd.sh
@@ -1,4 +1,4 @@
 dtStamp=$(date +%F_%X)
 VMFB_logfile="/data/log/VMFB_$(date +%F).log"
 touch "$VMFB_logfile"
-echo "$dtStamp	MOE	END	ME">> "$VMFB_logfile"
+echo "$dtStamp	MDE	END	ME">> "$VMFB_logfile"
